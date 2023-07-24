7bd1685fd6ae550a4d0069c45309bffb14626ad3
davidwerent <davidwerent@gmail.com>
2023-07-24 23:59:15 +0300

    ssl key

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, UploadFile, Form, Request, Response, File
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+import uvicorn
 
 templates = Jinja2Templates(directory='templates')
 
@@ -24,3 +25,11 @@ async def show_menu(request: Request):
 
     return templates.TemplateResponse('menu.html', {'request': request})
 
+if __name__ = 'main':
+    uvicorn.run(
+               app,
+               host="0.0.0.0",
+               port=443,
+               ssl_keyfile="/etc/letsencrypt/live/davidwerent.online/fullchain.pem",
+               ssl_certfile="/etc/letsencrypt/live/davidwerent.online/privkey.pem"
+               )
