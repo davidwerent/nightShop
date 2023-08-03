4cac888ae6a484e611ab15b24c04a27a03d46568
David Werent <i@davidwerent.ru>
2023-08-03 20:45:04 +0300

    send data 7 + user data

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
+import json
+import sqlite3
+from datetime import datetime
 
 bot = Bot('6105878178:AAGUaHuZ6stFOZNAfAtR26XxZo-Wn2qgkp8')
 dp = Dispatcher(bot)
 
-
+conn = sqlite3.connect('sqlite3.db')
+cursor = conn.cursor()
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
@@ -15,10 +19,27 @@ async def start(message: types.Message):
 
 @dp.message_handler(content_types=['web_app_data'])
 async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    print(res)
+    print(res[0])
+    print(res[1]["id"])
+
+    request = res
+    user_id = message.from_user.id
+    date = datetime.now()
+    address = res[0]['address']
+    phone = res[0]['phone']
+    name = res[0]['name']
+
+    res.pop(0)
+    total_sum = 0
+    for item in res:
+        print(item)
+        total_sum += item['price'] * item['count']
 
 
 
-    await message.answer(message.web_app_data.data)
+    await message.answer(res)
 
 
 executor.start_polling(dp)
