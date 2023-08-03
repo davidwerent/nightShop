c406fd77d08734e96bfaee9c615a094c9cdccb22
David Werent <i@davidwerent.ru>
2023-08-03 20:19:17 +0300

    send data 5 + user data

--- bot.py
+++ bot.py
@@ -4,11 +4,21 @@ from aiogram.types.web_app_info import WebAppInfo
 bot = Bot('6105878178:AAGUaHuZ6stFOZNAfAtR26XxZo-Wn2qgkp8')
 dp = Dispatcher(bot)
 
+
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Открыть магазин', web_app=WebAppInfo(url='https://davidwerent.online/menu')))
+    markup.add(types.KeyboardButton('Сделать заказ', web_app=WebAppInfo(url='https://davidwerent.online/menu')))
     await message.answer('Привет, покупатель!', reply_markup=markup)
+
+
+@dp.message_handler(content_types=['web_app_data'])
+async def web_app(message: types.Message):
+
+
+
+    await message.answer(message.web_app_data.data)
 
 
 executor.start_polling(dp)
